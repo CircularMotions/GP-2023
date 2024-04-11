946b00331d2ca12ae3c4e9415aaf2a53f090e826
CircularMotions <C23328021@mytudublin.ie>
2024-04-11 14:20:20 +0100

    added project folder

---
+++ Blorpboard/PlayLoop1.gd
@@ -0,0 +1,28 @@
+extends AudioStreamPlayer
+var loop
+
+func _ready():
+	loop = false
+
+func _on_finished():
+	if loop == true:
+		play()
+	
+func _on_drum_loop_1_button_toggled(toggled_on):
+	if loop == false:
+		loop = true
+		play()
+	elif loop == true:
+		loop = false
+		playing = false
+
+
+func _on_drum_loop_2_button_toggled(toggled_on):
+	if loop == true:
+		loop = false
+		playing = false
+
+func _on_drum_loop_3_button_toggled(toggled_on):
+	if loop == true:
+		loop = false
+		playing = false

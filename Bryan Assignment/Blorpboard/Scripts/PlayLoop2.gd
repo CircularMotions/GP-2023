09f48ac10aa4acae836162ff1133920e2a841d04
CircularMotions <C23328021@mytudublin.ie>
2024-04-12 12:58:04 +0100

    Drums can all change speed

--- Bryan Assignment/Blorpboard/Scripts/PlayLoop2.gd
+++ Bryan Assignment/Blorpboard/Scripts/PlayLoop2.gd
@@ -1,8 +1,12 @@
 extends AudioStreamPlayer
 var loop
+var pitch
+var slider
 
 func _ready():
 	loop = false
+	pitch = 1
+	slider = 100
 
 func _on_finished():
 	if loop == true:
@@ -27,3 +31,10 @@ func _on_drum_loop_3_button_toggled(toggled_on):
 	if loop == true:
 		loop = false
 		playing = false
+
+func _process(delta):
+	pitch_scale = pitch
+	pitch = slider/100
+
+func _on_h_slider_value_changed(value):
+	slider = value
